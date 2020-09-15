aecb4fd9a9a1a774fa6d3c7b8fed48d612dd0262
Ana Julia Lopes <anajulialopes@Anas-MacBook-Air.local>
2020-09-15 16:40:23 -0300

    Resolução dos exercícios de variáveis

---
+++ quinzena3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,77 @@
+// EXERCÍCIO 1
+
+// Será impresso o número 10 e depois será impresso o número 5.
+
+// EXERCÍCIO 2
+
+//Será impresso 10 20 10.
+
+// ------------------------------- //
+
+//EXERCÍCIOS DE CÓDIGO//
+
+//1.
+//a.
+const nome = " "
+// //b.
+let idade = " "
+// //c.
+console.log(typeof (nome, idade))
+//d.
+//Foi impresso o tipo string, devido ao uso de " " no valor da variável.
+//e.
+const nome = prompt("Qual o seu nome?")
+console.log(nome)
+
+let idade = prompt("Qual sua idade?")
+console.log(idade)
+
+console.log(typeof (nome, idade))
+
+//f.O tipo permanece string pois no valor atribuido temos um prompt com strigs.
+//g.
+console.log("Olá", nome, "você tem", idade, "anos")
+
+
+
+//2.
+
+let cor = prompt("Qual sua cor favorita?")
+console.log(cor)
+let animais = prompt("Você tem animais de estimação?")
+console.log(animais)
+let altura = prompt("Qual sua altura?")
+console.log(altura)
+let olhos = prompt("Qual a cor do seus olhos?")
+console.log(olhos)
+let irmaos = prompt("Quantos irmãos você tem?")
+console.log(irmaos)
+
+//3.
+//a.
+let comida = ["batata", "milho", "chocolate", "strogonoff", "brócolis"]
+console.log(comida)
+// // //b.
+console.log("Essas são minhas comidas preferidas:")
+console.log(comida[0])
+console.log(comida[1])
+console.log(comida[2])
+console.log(comida[3])
+console.log(comida[4])
+// //c.
+let preferida = prompt("Qual sua comida preferida?")
+console.log(comida[0])
+console.log(preferida)
+console.log(comida[2])
+console.log(comida[3])
+console.log(comida[4])
+
+//4.
+//a.
+let perguntas = ["Você está com fome?", "Você está com sono?", "Você usa óculos?"]
+let respostas = [false, true, true]
+//b.
+console.log(perguntas[0], respostas[0])
+console.log(perguntas[1], respostas[1])
+console.log(perguntas[2], respostas[2])
+
