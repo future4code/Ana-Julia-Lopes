60adc0629c331c6b6e668d2415de46154666d9e4
Ana Julia Lopes <anajulialopes@Anas-MacBook-Air.local>
2020-09-17 16:53:24 -0300

    Resolução dos exercícios de operadores

---
+++ quinzena3/operadores/boilerplate/index.js
@@ -0,0 +1,44 @@
+// console.log("Oie, eu só sirvo pra saber se as coisas funcionaram!")
+
+//Exercício 1
+//a. false;
+//b. false;
+//c. falese;
+//d. booleno.
+
+//Exercício 2
+//a. undefined;
+//b. null;
+//c. 10;
+//d. 0;
+//e.  ;
+//f. 
+
+//Exercicio de código
+
+//Exercício 1
+//a.
+// let minhaIdade = prompt("Qual sua idade?")
+// let number = Number(minhaIdade)
+
+// console.log(number)
+
+// // //b.
+// let melhorAmigx = prompt("Qual idade do seu melhor amigx?")
+// let idade = Number(melhorAmigx)
+
+// console.log(idade)
+
+// //c.
+// let condicao = minhaIdade !== melhorAmigx
+
+// console.log("Sua idade é maior do que a do seu melhor amigo?", condicao)
+
+//d.
+// let minhaIdade = 26
+// let melhorAmigx = 25
+// let resultado = minhaIdade - melhorAmigx
+
+// console.log("A diferença é igual a", resultado)
+
+//Exercício 2
