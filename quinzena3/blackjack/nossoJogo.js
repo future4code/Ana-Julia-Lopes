de13141d51e0772a2ba4001902d157d2f8f2e005
Ana Julia Lopes <anajulialopes@Anas-MacBook-Air.local>
2020-09-27 18:39:15 -0300

    Projeto blackjack

--- quinzena3/blackjack/nossoJogo.js
+++ quinzena3/blackjack/nossoJogo.js
@@ -12,5 +12,29 @@
  */
 
 console.log("Bem vindo ao jogo de Blackjack!")
+
+while (confirm("Quer iniciar uma nova rodada?")) {
+   const carta1Jogador = comprarCarta()
+   const carta2Jogador = comprarCarta()
+
+   const carta1Computador = comprarCarta()
+   const carta2Computador = comprarCarta()
+
+   const pontuacaoJogador = carta1Jogador.valor + carta2Jogador.valor
+   const pontuacaoComputador = carta1Computador.valor + carta2Computador.valor
+
+   console.log("Usuário - cartas: ", carta1Jogador.texto, " ", carta2Jogador.texto, " - pontuação: ", pontuacaoJogador)
+   console.log("Computador - cartas: ", carta1Computador.texto, " ", carta2Computador.texto, " - pontuação: ", pontuacaoComputador)
+
+   if (pontuacaoJogador > pontuacaoComputador) {
+      console.log("O usuário ganhou!")
+   } else if (pontuacaoComputador > pontuacaoJogador) {
+      console.log("O computador ganhou!")
+   } else {
+      console.log("Empate!")
+   }
+}
+
+console.log("O jogo acabou")
    
 
