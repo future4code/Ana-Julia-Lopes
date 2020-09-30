7f9ed01079b8676a287ef8ae08d3447bff42803f
Ana Julia Lopes <anajulialopes@Anas-MacBook-Air.local>
2020-09-29 23:57:59 -0300

    Exercícios de funções

---
+++ quinzena4/funcoes/index.js
@@ -0,0 +1,83 @@
+// console.log("Oie, eu só sirvo pra saber se as coisas funcionaram!")
+
+//-------------------> EXERCÍCIO 1  
+
+// A.
+//10
+//50
+
+//B.
+//Nada apareceria, a função só retornaria.
+
+//---------------------> EXERCÍCIO 2
+
+//A.
+//"Darvas", "Goli", "João"
+
+//B.
+//"Amanda", "Caio"
+
+//--------------------> EXERCÍCIO 3
+
+//A.
+//A função recebe um array como parâmetro, e adiciona ao arrayFinal números divisiveis por 2 e multiplicados por eles mesmos.
+
+
+//--------------------> EXERCÍCIO 4
+
+//A. 
+// const informacoesPessoais = () => {
+//     console.log("Eu sou Ana Julia, tenho 26 anos, moro em Avaré e sou estudante")
+// }
+
+// informacoesPessoais()
+
+//B.
+
+// const informacoesPessoais = (nome, idade, cidade, estudante) => {
+//     let souNaoSou 
+//     if (estudante) {
+//         souNaoSou = "sou" 
+//     } else {
+//         souNaoSou = "não sou"
+//     }
+//     console.log("Eu sou " , nome,",", "tenho ", idade, "anos ", ",", "moro em ", cidade, "e", souNaoSou, "estudante")
+// }
+
+// informacoesPessoais("Ana Julia", Number(26), "Avaré", true)
+
+
+//------------------> EXERCÍCIO 5
+
+//A.
+
+// let numeros = (numero1, numero2) => {
+//     return numero1 + numero2
+// }
+
+// console.log(numeros (5,2))
+
+//B.
+
+// let numeros = (numero1, numero2) => {
+//     return numero1 >= numero2
+// }
+
+// numeros (20, 12)
+
+//C.
+
+// let funcao = (frase) => {
+//     for (let i = 0; i < 10; i++){
+//         console.log(frase)
+//     }
+// }
+
+// funcao ("Oi eu sou uma string")
+
+//------------> EXERCÍCIO 6
+
+//A.
+
+const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
