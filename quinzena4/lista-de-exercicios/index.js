6608d66549cf832602c7bcf1df89eb411e6ad880
Ana Julia Lopes <anajulialopes@Anas-MacBook-Air.local>
2020-10-12 15:17:45 -0300

    Exercício número 2 de interpretação de código - Investimentos

---
+++ quinzena4/lista-de-exercicios/index.js
@@ -0,0 +1,15 @@
+// ------------------ EXERCÍCIOS DE INTERPRETAÇÃO ------------------//
+
+// 1.
+// A função recebe como parâmetro um valor em dólar e retorna o valor em reais. Pegando o valor em doláres e multiplicando pela cotação.
+// 553.
+
+//------------------------------------------------------------------
+
+// 2.
+// A função investeDinheiro tem 2 parâmetros (tipoDeInvestimento e valor), o usuário vai escolher o tipo de investimento que deseja fazer e o valor, o switch case toma a desição
+// com base nos parâmetros escolhidos, retornando o valorAposInvestimento.
+// 165
+// undefined
+
+//-------------------------------------------------------------------
